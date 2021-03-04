4fcf5ce98dce59ea42e4fd1b08eb49458a8e6c20
Gilberto <78664060+XxGiillxX@users.noreply.github.com>
2021-03-04 19:32:17 -0300

    Update Principal.java

--- NullBank1/src/model/Principal.java
+++ NullBank1/src/model/Principal.java
@@ -6,18 +6,21 @@ public class Principal {
 
 	public static void main(String[] args) {
 		
-		
+		// criação dos objetos
 		Pessoa cliente = new Pessoa();
 		cliente.setNome("Gilberto");
 		cliente.setCPF("6650654");
 		
-		
 		Emprestimo em = new Emprestimo();
 		ContaCorrente cc1 = new ContaCorrente();
 		ContaPoupanca cp1 = new ContaPoupanca();
+		
+		// atribuindo tipo de conta ao objeto pessoa
 		cc1.setPessoa(cliente);
 		cp1.setPessoa(cliente);
 		em.setPessoa(cliente);
+		
+		//usando atributos
 		cc1.abrirConta();
 		cp1.abrirConta();
 		cp1.bonus();
